073bf81358f372737f6959e2c0c7c68a865608b3
Rejaul Islam <rejaulislammr25@gmail.com>
2024-02-09 20:55:34 +0600

    update 1.0.6

--- js/main.js
+++ js/main.js
@@ -9,7 +9,16 @@ $(document).ready(function () {
    
     });
 
-    $('.display_book_slider').slick({
+  
+    
+  });
+
+
+  /* this is main display's slider */
+  $(document).ready(function(){
+
+    // main display slider 1
+    $('#fontSlider1').slick({
       infinite: true,
       slidesToShow: 4,
       autoplay: true,
@@ -42,7 +51,7 @@ $(document).ready(function () {
               slidesToShow: 2,
               slidesToScroll: 1,
               arrows: false,
-              dots: true,
+              dots: false,
             }
           },
         {
@@ -51,31 +60,143 @@ $(document).ready(function () {
             slidesToShow: 2,
             slidesToScroll: 1,
             arrows: false,
-            dots: true,
+            dots: false,
             
           }
         }
        
       ]
 
+
+      
+
    
     });
+    // main display slider 2
+    $('#fontSlider2').slick({
+      infinite: true,
+      slidesToShow: 4,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            cssEase: 'linear',
+            // dots: true
+          }
+        },
 
-    // when scrollamount > 100 header position fixed in top 0
-/*
-    $(window).scroll(function(){
-      if($(this).scrollTop() > 100) {
-          $('#myHeader').css({'position':'fixed', 'top':'0'});
-      } else {
-          $('#myHeader').css({'position':'static'});
-      }
-  });
-  */
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+          }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false,
+              dots: false,
+            }
+          },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            
+          }
+        }
+       
+      ]
 
+
+      
+
+   
+    });
+    // main display slider 3
+    $('#fontSlider3').slick({
+      infinite: true,
+      slidesToShow: 4,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            cssEase: 'linear',
+            // dots: true
+          }
+        },
+
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+          }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false,
+              dots: false,
+            }
+          },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            
+          }
+        }
+       
+      ]
+
+
+      
+
+   
+    });
   
 
+        // Custom button click events
+        $('.prev_btn').click(function(){
+          var sliderId = $(this).data('slider');
+          $(sliderId).slick('slickPrev');
+        });
     
+        $('.next_btn').click(function(){
+          var sliderId = $(this).data('slider');
+          $(sliderId).slick('slickNext');
+        });
+
   });
+  /* this is main display's slider */
 
   
 
